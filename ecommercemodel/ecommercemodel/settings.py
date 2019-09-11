ffab8757e9c196bfb663ccf730ef63efa5cb4ad0
Dhaniel Ricardo <dhanielricardo@nazaria.com.br>
2019-09-11 09:35:11 -0300

    Configs

--- ecommercemodel/ecommercemodel/settings.py
+++ ecommercemodel/ecommercemodel/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_RROT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
