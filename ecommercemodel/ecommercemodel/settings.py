ae109f191514f9b7e047b98e9c6fc61b31a11894
Dhaniel Ricardo <dhanielricardo@nazaria.com.br>
2019-09-11 16:58:32 -0300

    Teste heroku 9

--- ecommercemodel/ecommercemodel/settings.py
+++ ecommercemodel/ecommercemodel/settings.py
@@ -128,10 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
