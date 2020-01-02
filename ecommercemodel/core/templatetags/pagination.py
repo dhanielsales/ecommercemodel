da606974955a6966d741a48fbd8eff1a57968e54
Dhaniel Ricardo <dhanielricardo@nazaria.com.br>
2020-01-02 17:56:51 -0300

    Design

--- ecommercemodel/core/templatetags/pagination.py
+++ ecommercemodel/core/templatetags/pagination.py
@@ -2,7 +2,7 @@ from django.template import Library
 
 register = Library()
 
-@register.inclusion_tag('pagination.html')
+@register.inclusion_tag('core/pagination.html')
 def pagination(request, paginator, page_obj):
     context = {}
     context['paginator'] = paginator
