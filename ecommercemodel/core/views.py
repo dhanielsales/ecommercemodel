b1aaefb1a86468617cd77830ae6dadc193306bda
Dhaniel Ricardo <dhanielricardo@nazaria.com.br>
2020-01-23 17:54:05 -0300

    Model de Login com Ajax

--- ecommercemodel/core/views.py
+++ ecommercemodel/core/views.py
@@ -5,20 +5,32 @@ import datetime
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View, TemplateView, CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.utils.translation import gettext_lazy 
 
 from .forms import ContactForm
 from catalog.models import Product, Category
 
 User = get_user_model()
 
-class IndexView(TemplateView):  
+class MyAuthForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': gettext_lazy(
+            "Por favor, entre com um E-mail e Senha corretos. Note que ambos "
+            "os campos diferenciam maiúsculas e minúsculas."
+        ),
+        'inactive': gettext_lazy("Essa conta está inativa."),
+    }
+
+class IndexView(TemplateView, LoginView):  
 
     template_name = "core/index.html"
     model = Product
+    authentication_form = MyAuthForm
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
